68f8e0d8247ed57c2e4dd1380ba60b9d4a1846fd
ringeringeraja <joaosan177@gmail.com>
2024-03-26 15:40:49 -0300

    fix(aeria-ui): missing exports

---
+++ .changeset/ninety-beans-beg.md
@@ -0,0 +1,5 @@
+---
+"aeria-ui": patch
+---
+
+Fix reexports
--- packages/aeria-ui/src/index.ts
+++ packages/aeria-ui/src/index.ts
@@ -1,24 +1,5 @@
-export {
-  type MenuSchema,
-  type MenuNode,
-  type AppOptions,
-  type CollectionStore,
-  defineOptions,
-  condenseItem,
-  createCollectionStore,
-  useBreakpoints,
-  useClipboard,
-  useAction,
-  useStoreActions,
-  useNavbar,
-  useApp,
-  request,
-  API_URL,
-
-} from '@aeria-ui/web'
-
-export {
-  t, createI18n, setLocale, getLocale, defineLocale,
-} from '@aeria-ui/i18n'
+export * from '@aeria-ui/web'
+export * from '@aeria-ui/i18n'
 export * from '@aeria-ui/state-management'
 export * from '@aeria-ui/ui'
+
