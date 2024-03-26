fa26205e5ab5cb0ccc1bf8d4bb71f71dd9ef4af9
ringeringeraja <joaosan177@gmail.com>
2024-03-26 03:24:24 -0300

    fix(web): remove side effects

---
+++ .changeset/angry-rivers-dance.md
@@ -0,0 +1,5 @@
+---
+"@aeria-ui/web": patch
+---
+
+Remove side effects
--- packages/web/src/composables/use-breakpoints.ts
+++ packages/web/src/composables/use-breakpoints.ts
@@ -2,22 +2,32 @@ import { ref, computed } from 'vue'
 
 const widthMatches = (size: string) => window.matchMedia(`(min-width: ${size})`).matches
 
-const viewport = ref({
-  width: window.innerWidth,
-  height: window.innerHeight,
-})
-
-const breakpoints = computed(() => ({
-  width: viewport.value.width,
-  height: viewport.value.height,
-  md: widthMatches('768px'),
-  lg: widthMatches('1024px'),
-  xl: widthMatches('1280px'),
-}))
-
 let listenerAttached = false
 
 export const useBreakpoints = () => {
+  if( typeof window === 'undefined' ) {
+    return {} as {
+      width: number
+      height: number
+      md: number
+      lg: number
+      xl: number
+    }
+  }
+
+  const viewport = ref({
+    width: window.innerWidth,
+    height: window.innerHeight,
+  })
+
+  const breakpoints = computed(() => ({
+    width: viewport.value.width,
+    height: viewport.value.height,
+    md: widthMatches('768px'),
+    lg: widthMatches('1024px'),
+    xl: widthMatches('1280px'),
+  }))
+
   if( !listenerAttached ) {
     window.addEventListener('resize', () => {
       viewport.value.width = window.innerWidth
@@ -29,3 +39,4 @@ export const useBreakpoints = () => {
 
   return breakpoints
 }
+
