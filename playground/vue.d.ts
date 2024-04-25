95420e37d5612c21715d0ec5c6389f3356d7c860
ringeringeraja <joaosan177@gmail.com>
2024-04-25 03:10:11 -0300

    chore: cleanup

--- playground/vue.d.ts
+++
@@ -1,8 +0,0 @@
-/// <reference types="vue/macros-global" />
-
-/* eslint-disable */
-declare module '*.vue' {
-  import type { DefineComponent } from 'vue'
-  const component: DefineComponent<{}, {}, any>
-  export default component
-}
