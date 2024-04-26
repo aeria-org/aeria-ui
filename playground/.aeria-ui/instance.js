40cfe8e6244041f0adfb5b72089dc2aae76d6a4c
ringeringeraja <joaosan177@gmail.com>
2024-04-26 00:04:55 -0300

    chore: playground

--- playground/.aeria-ui/instance.js
+++ playground/.aeria-ui/instance.js
@@ -19,9 +19,6 @@ export default {
     ]
   },
   vite: {
-    // resolve: {
-    //   preserveSymlinks: true,
-    // },
     plugins: [
       vueRouter({
         dts: './.aeria-ui/typed-router.d.ts'
