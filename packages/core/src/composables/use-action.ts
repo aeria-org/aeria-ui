6a26cdbad511b91e3a95e2108456d3cf8131e225
ringeringeraja <joaosan177@gmail.com>
2024-08-23 01:44:24 -0300

    fix(core): incorrect import

--- packages/core/src/composables/use-action.ts
+++ packages/core/src/composables/use-action.ts
@@ -1,7 +1,7 @@
 import type { Router } from 'vue-router'
 import type { CollectionAction, Result } from '@aeriajs/types'
 import type { GlobalStateManager } from '@aeria-ui/state-management'
-import type { CollectionStore } from '@aeria-ui/core'
+import type { CollectionStore } from '../state/collection.js'
 import { useStore } from '@aeria-ui/state-management'
 import { reactive } from 'vue'
 import { deepClone } from '@aeriajs/common'
