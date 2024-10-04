5b2c04127deb5cb28313d0422b4aee37e7b29e64
ringeringeraja <joaosan177@gmail.com>
2024-10-04 15:46:25 -0300

    fix(aeria-ui): re-export from `@aeria-ui/utils`

---
+++ .changeset/two-cups-decide.md
@@ -0,0 +1,5 @@
+---
+"aeria-ui": patch
+---
+
+Re-export `condenseItem()`
--- packages/aeria-ui/src/index.ts
+++ packages/aeria-ui/src/index.ts
@@ -3,4 +3,5 @@ export * from '@aeria-ui/core'
 export * from '@aeria-ui/i18n'
 export * from '@aeria-ui/state-management'
 export * from '@aeria-ui/ui'
+export * from '@aeria-ui/utils'
 
