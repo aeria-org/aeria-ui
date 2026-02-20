0518cac491b41af16afd3f973e04e09129958673
ringeringeraja <joaosan177@gmail.com>
2026-02-20 11:59:17 -0300

    chore: add hook script

---
+++ scripts/hook.sh
@@ -0,0 +1,79 @@
+#!/bin/sh
+
+UNDO=
+ROOT="$(echo -n "$0" | sed -r 's/\/scripts\/([^$]+)//')"
+
+function is_nested() {
+  occurr="$(echo -n "$1" | tr '/' '\n' | grep -c 'node_modules')"
+  [ $occurr -gt 1 ]
+}
+
+function replace_with_symlink() {
+  origin="$1"
+  target="$ROOT/$2"
+
+  echo "$origin"
+
+  if [ -d "$origin" ]; then
+    mv "$origin" "${origin}_bkp"
+    ln -s "$target" "$origin"
+
+    echo "$origin -> $target"
+  fi
+}
+
+function revert_symlink() {
+  origin="$1_bkp"
+  target="$1"
+
+  echo "$origin"
+
+  if [ -d "$origin" ]; then
+    rm -rf "$target"
+    mv "$origin" "$target"
+
+    echo "$origin -> $target"
+  fi
+}
+
+while getopts "hu" arg; do
+  case "$arg" in
+    h)
+      echo "to hook a project: $0"
+      echo "to undo: $0 -u"
+      ;;
+    u)
+      UNDO=1
+      echo "undoing..."
+      ;;
+  esac
+done
+
+for d in $(find -type d -name vue-router); do
+  if is_nested "$d"; then
+    continue
+  fi
+
+  if [ -n "$UNDO" ]; then
+    revert_symlink "$d"
+  else
+    replace_with_symlink "$d" "node_modules/vue-router"
+  fi
+
+done
+
+for d in $(find -type d -name @aeria-ui); do
+  if is_nested "$d"; then
+    continue
+  fi
+
+  if [ -n "$UNDO" ]; then
+    revert_symlink "$d"
+  else
+    replace_with_symlink "$d/core" "packages/core"
+    replace_with_symlink "$d/ui" "packages/ui"
+  fi
+
+
+done
+
